f905392a6a9d6e316f72eb389e8a4cd023ff00cc
Donna M. Ada <donnaada@icloud.com>
2023-04-11 07:08:54 +1000

    created components folder for better organization

---
+++ src/components/HornedBeast.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class HornedBeast extends React.Component{
+  render(){
+    return(
+      <>
+        <h2>Title: {this.props.title}</h2>
+        <img src={this.props.imageUrl} alt={this.props.description} title={this.props.title} />
+        <p>Description: {this.props.description}</p>
+      </>
+    )
+  }
+}
+
+export default HornedBeast;
